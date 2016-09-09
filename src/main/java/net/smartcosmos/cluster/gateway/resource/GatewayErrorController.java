6a03a8cbbfd7a326b533e5133913237afaa85606
Will Schneider <will.schneider@smartrac-group.com>
2016-09-09 07:31:02 -0400

    OBJECTS-977: implement custom ErrorController to capture errors

--- src/main/java/net/smartcosmos/cluster/gateway/resource/GatewayErrorController.java
+++ src/main/java/net/smartcosmos/cluster/gateway/resource/GatewayErrorController.java
@@ -1,5 +1,8 @@
 package net.smartcosmos.cluster.gateway.resource;
 
+import javax.servlet.http.HttpServletRequest;
+
+import com.netflix.zuul.context.RequestContext;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.boot.autoconfigure.web.ErrorController;
@@ -20,13 +23,38 @@ import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 @PreAuthorize("permitAll()")
 public class GatewayErrorController implements ErrorController {
 
-    public static final String ERROR_PATH = "/error";
+    private static final String ERROR_PATH = "/error";
+    private static final String ATTR_ERROR_EXCEPTION = "error.exception";
+    public static final String ATTR_ERROR_MESSAGE = "error.message";
+    public static final String ATTR_JAVAX_SERVLET_ERROR_STATUS_CODE = "javax.servlet.error.status_code";
+    public static final String ATTR_JAVAX_SERVLET_ERROR_MESSAGE = "javax.servlet.error.message";
+    public static final String ATTR_ERROR_STATUS_CODE = "error.status_code";
 
     @RequestMapping(value = ERROR_PATH)
     public ResponseEntity<?> error() {
 
-        return ResponseEntity.status(INTERNAL_SERVER_ERROR)
-            .build();
+        try {
+            RequestContext ctx = RequestContext.getCurrentContext();
+            HttpServletRequest request = ctx.getRequest();
+
+            int statusCode = (Integer) ctx.get(ATTR_ERROR_STATUS_CODE);
+
+            if (ctx.containsKey(ATTR_ERROR_EXCEPTION)) {
+                Object errorException = ctx.get(ATTR_ERROR_EXCEPTION);
+            }
+
+            if (ctx.containsKey(ATTR_ERROR_MESSAGE)) {
+                String message = (String) ctx.get(ATTR_ERROR_MESSAGE);
+                request.setAttribute(ATTR_JAVAX_SERVLET_ERROR_MESSAGE, message);
+            }
+
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR)
+                .build();
+        } catch (Throwable t) {
+            log.info("Exception, cause: {}", t.toString());
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR)
+                .body(t.toString());
+        }
     }
 
     @Override
