0593735e22d12f3757ba5d9009e7f1c288a30926
Andreas Siegel <andreas.siegel@smartrac-group.com>
2016-10-19 13:01:39 +0200

    Add tests for BadCredentialsException handling

--- src/test/java/net/smartcosmos/cluster/gateway/filters/PreAuthorizationFilterTest.java
+++ src/test/java/net/smartcosmos/cluster/gateway/filters/PreAuthorizationFilterTest.java
@@ -11,17 +11,21 @@ import org.mockito.runners.MockitoJUnitRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.security.oauth2.proxy.ProxyAuthenticationProperties;
+import org.springframework.security.authentication.BadCredentialsException;
 
 import net.smartcosmos.cluster.gateway.AuthenticationClient;
 
 import static ch.qos.logback.classic.Level.WARN;
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 @SuppressWarnings("unchecked")
 @RunWith(MockitoJUnitRunner.class)
@@ -87,6 +91,26 @@ public class PreAuthorizationFilterTest {
     }
 
     @Test
+    public void thatRunLogsWithWarnLevelInCaseOfBadCredentialsException() {
+
+        final String username = "someUser";
+        final String password = "someArbitraryTestingPassw0rd";
+
+        doReturn(new String[] { username, password }).when(filter)
+            .getAuthenticationCredentials();
+
+        when(authenticationClient.getOauthToken(eq(username), eq(password))).thenThrow(new BadCredentialsException("someException"));
+        doNothing().when(filter)
+            .setErrorResponse(any(), any());
+
+        filter.run();
+
+        verify(mockAppender, times(1)).doAppend(captorLoggingEvent.capture());
+        LoggingEvent loggingEvent = captorLoggingEvent.getValue();
+        assertEquals(WARN, loggingEvent.getLevel());
+    }
+
+    @Test
     public void thatRunLogsCorrectMessageInCaseOfException() {
 
         final String username = "someUser";
@@ -107,6 +131,28 @@ public class PreAuthorizationFilterTest {
     }
 
     @Test
+    public void thatRunLogsCorrectMessageInCaseOfBadCredentialsException() {
+
+        final String username = "someUser";
+        final String password = "someArbitraryTestingPassw0rd";
+
+        doReturn(new String[] { username, password }).when(filter)
+            .getAuthenticationCredentials();
+
+        when(authenticationClient.getOauthToken(eq(username), eq(password))).thenThrow(new BadCredentialsException("someException"));
+        doNothing().when(filter)
+            .setErrorResponse(any(), any());
+
+        filter.run();
+
+        verify(mockAppender, times(1)).doAppend(captorLoggingEvent.capture());
+        LoggingEvent loggingEvent = captorLoggingEvent.getValue();
+        assertNotNull(loggingEvent.getMessage());
+        assertTrue(loggingEvent.getMessage()
+                       .startsWith("Authentication request failed."));
+    }
+
+    @Test
     public void thatRunLogsNoPasswordInCaseOfException() {
 
         final String username = "someUser";
@@ -116,6 +162,27 @@ public class PreAuthorizationFilterTest {
             .getAuthenticationCredentials();
 
         when(authenticationClient.getOauthToken(eq(username), eq(password))).thenThrow(new RuntimeException("someException"));
+
+        filter.run();
+
+        verify(mockAppender, times(1)).doAppend(captorLoggingEvent.capture());
+        LoggingEvent loggingEvent = captorLoggingEvent.getValue();
+        assertFalse(loggingEvent.getFormattedMessage()
+                        .contains(password));
+    }
+
+    @Test
+    public void thatRunLogsNoPasswordInCaseOfBadCredentialsException() {
+
+        final String username = "someUser";
+        final String password = "someArbitraryTestingPassw0rd";
+
+        doReturn(new String[] { username, password }).when(filter)
+            .getAuthenticationCredentials();
+
+        when(authenticationClient.getOauthToken(eq(username), eq(password))).thenThrow(new BadCredentialsException("someException"));
+        doNothing().when(filter)
+            .setErrorResponse(any(), any());
 
         filter.run();
 
@@ -185,4 +252,24 @@ public class PreAuthorizationFilterTest {
         assertTrue(loggingEvent.getFormattedMessage()
                        .contains(exception.toString()));
     }
+
+    @Test
+    public void thatRunCallsSetErrorResponseInCaseOfBadCredentialsException() {
+
+        final String username = "someUser";
+        final String password = "someArbitraryTestingPassw0rd";
+        final String message = "exception message";
+
+        doReturn(new String[] { username, password }).when(filter)
+            .getAuthenticationCredentials();
+
+        when(authenticationClient.getOauthToken(eq(username), eq(password))).thenThrow(new BadCredentialsException(message));
+        doNothing().when(filter)
+            .setErrorResponse(any(), any());
+
+        filter.run();
+
+        verify(filter, times(1)).setErrorResponse(eq(UNAUTHORIZED), eq(message));
+    }
+
 }
